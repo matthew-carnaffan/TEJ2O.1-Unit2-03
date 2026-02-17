631fb561fbc9653b450b776a203db3f80c88e298
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-17 13:20:11 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: Feb 2026
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.showString('A rectangle has dimensions 5 cm & 3 cm')
+basic.showString('The perimeter would be: ' + (2*(5+3)).toString() + ' cm.')
+basic.showString('and the Area is' + (5*3).toString() + ' cm^2.')
